86bfd5c3d8b7ceb68b3c256446ba0a3bea9dbfce
care-ruthwik <ruthwik.soudry@v.curefit.com>
2020-03-21 19:17:37 +0530

    push ing changes

--- src/MenuBar.js
+++ src/MenuBar.js
@@ -4,11 +4,16 @@ import { _openPlayStore } from './RouteFunction';
 import AristotleLogo from "./assets/aristotle_logo.png";
 
 function MenuBar(props) {
+
+    const clickHandler = () => {
+        window.open("http://digitalaristotle.ai/")
+    }
     return (
         <div className="menubar-container">
             <div className="container">
                 <div className=" menubar-sub-wrapper row">
-                    <img className="aristotle-logo" alt="img" src={AristotleLogo} />
+                    <img onClick={clickHandler}
+                        className="aristotle-logo" alt="img" src={AristotleLogo} />
                     <div className="menubar-right">
                         <div className="download-app-wrapper">
                             <div onClick={_openPlayStore} className="get-the-app">DOWNLOAD APP</div>
